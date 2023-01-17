e5546781b2b8bbd0c1355266034b03d40580bfba
Shivraj <shivrajofficial102@gmail.com>
2023-01-17 17:45:00 +0530

    modified merge sort and SortingVisualizer component

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { getMergeSortAnimations } from "../sortingAlgorithms/sortingAlgorithms.js";
 import "./SortingVisualizer.css";
 
 // Change this value for the speed of the animations.
-const ANIMATION_SPEED_MS = 110;
+const ANIMATION_SPEED_MS = 10;
 
 // Change this value for the number of bars (value) in the array.
 const NUMBER_OF_ARRAY_BARS = 50;
@@ -14,29 +15,121 @@ const PRIMARY_COLOR = "turquoise";
 // This is the color of array bars that are being compared throughout the animations.
 const SECONDARY_COLOR = "red";
 
-export default class SortingVisualizer extends React.Component {
-  constructor(props) {
-    super(props);
+// export default class SortingVisualizer extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      array: [],
-    };
-  }
+//     this.state = {
+//       array: [],
+//     };
+//   }
 
-  componentDidMount() {
-    this.resetArray();
-  }
+//   componentDidMount() {
+//     this.resetArray();
+//   }
 
-  resetArray() {
+//   resetArray() {
+//     const array = [];
+//     for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+//       array.push(randomIntFromInterval(5, 730));
+//     }
+//     this.setState({ array });
+//   }
+
+//   mergeSort() {
+//     const animations = getMergeSortAnimations(this.state.array);
+//     for (let i = 0; i < animations.length; i++) {
+//       const arrayBars = document.getElementsByClassName("array-bar");
+//       const isColorChange = i % 3 !== 2;
+//       if (isColorChange) {
+//         const [barOneIdx, barTwoIdx] = animations[i];
+//         const barOneStyle = arrayBars[barOneIdx].style;
+//         const barTwoStyle = arrayBars[barTwoIdx].style;
+//         const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+//         setTimeout(() => {
+//           barOneStyle.backgroundColor = color;
+//           barTwoStyle.backgroundColor = color;
+//         }, i * ANIMATION_SPEED_MS);
+//       } else {
+//         setTimeout(() => {
+//           const [barOneIdx, newHeight] = animations[i];
+//           const barOneStyle = arrayBars[barOneIdx].style;
+//           barOneStyle.height = `${newHeight}px`;
+//         }, i * ANIMATION_SPEED_MS);
+//       }
+//     }
+//   }
+
+//   quickSort() {
+//     // We leave it as an exercise to the viewer of this code to implement this method.
+//   }
+
+//   heapSort() {
+//     // We leave it as an exercise to the viewer of this code to implement this method.
+//   }
+
+//   bubbleSort() {
+//     // We leave it as an exercise to the viewer of this code to implement this method.
+//   }
+
+//   // NOTE: This method will only work if your sorting algorithms actually return
+//   // the sorted arrays; if they return the animations (as they currently do), then
+//   // this method will be broken.
+//   testSortingAlgorithms() {
+//     for (let i = 0; i < 100; i++) {
+//       const array = [];
+//       const length = randomIntFromInterval(1, 1000);
+//       for (let i = 0; i < length; i++) {
+//         array.push(randomIntFromInterval(-1000, 1000));
+//       }
+//       const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
+//       const mergeSortedArray = getMergeSortAnimations(array.slice());
+//       console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray));
+//     }
+//   }
+
+//   render() {
+//     const { array } = this.state;
+
+//     return (
+//       <div className="array-container">
+//         {array.map((value, idx) => (
+//           <div
+//             className="array-bar"
+//             key={idx}
+//             style={{
+//               backgroundColor: PRIMARY_COLOR,
+//               height: `${value}px`,
+//             }}
+//           ></div>
+//         ))}
+//         <button onClick={() => this.resetArray()}>Generate New Array</button>
+//         <button onClick={() => this.mergeSort()}>Merge Sort</button>
+//         <button onClick={() => this.quickSort()}>Quick Sort</button>
+//         <button onClick={() => this.heapSort()}>Heap Sort</button>
+//         <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+//         <button onClick={() => this.testSortingAlgorithms()}>
+//           Test Sorting Algorithms (BROKEN)
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+export default function Example() {
+  const [array, setArray] = useState([]);
+
+  useEffect(() => {
+    resetArray();
+  }, []);
+  function resetArray() {
     const array = [];
     for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
       array.push(randomIntFromInterval(5, 730));
     }
-    this.setState({ array });
+    setArray(array);
   }
-
-  mergeSort() {
-    const animations = getMergeSortAnimations(this.state.array);
+  function mergeSort() {
+    const animations = getMergeSortAnimations(array);
     for (let i = 0; i < animations.length; i++) {
       const arrayBars = document.getElementsByClassName("array-bar");
       const isColorChange = i % 3 !== 2;
@@ -58,23 +151,18 @@ export default class SortingVisualizer extends React.Component {
       }
     }
   }
+  //  quickSort() {
+  //     // We leave it as an exercise to the viewer of this code to implement this method.
+  //   }
 
-  quickSort() {
-    // We leave it as an exercise to the viewer of this code to implement this method.
-  }
+  //   heapSort() {
+  //     // We leave it as an exercise to the viewer of this code to implement this method.
+  //   }
 
-  heapSort() {
-    // We leave it as an exercise to the viewer of this code to implement this method.
-  }
-
-  bubbleSort() {
-    // We leave it as an exercise to the viewer of this code to implement this method.
-  }
-
-  // NOTE: This method will only work if your sorting algorithms actually return
-  // the sorted arrays; if they return the animations (as they currently do), then
-  // this method will be broken.
-  testSortingAlgorithms() {
+  //   bubbleSort() {
+  //     // We leave it as an exercise to the viewer of this code to implement this method.
+  //   }
+  function testSortingAlgorithms() {
     for (let i = 0; i < 100; i++) {
       const array = [];
       const length = randomIntFromInterval(1, 1000);
@@ -86,33 +174,33 @@ export default class SortingVisualizer extends React.Component {
       console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray));
     }
   }
-
-  render() {
-    const { array } = this.state;
-
-    return (
-      <div className="array-container">
-        {array.map((value, idx) => (
-          <div
-            className="array-bar"
-            key={idx}
-            style={{
-              backgroundColor: PRIMARY_COLOR,
-              height: `${value}px`,
-            }}
-          ></div>
-        ))}
-        <button onClick={() => this.resetArray()}>Generate New Array</button>
-        <button onClick={() => this.mergeSort()}>Merge Sort</button>
-        <button onClick={() => this.quickSort()}>Quick Sort</button>
-        <button onClick={() => this.heapSort()}>Heap Sort</button>
-        <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
-        <button onClick={() => this.testSortingAlgorithms()}>
-          Test Sorting Algorithms (BROKEN)
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="array-container">
+      {array.map((value, idx) => (
+        <div
+          className="array-bar"
+          key={idx}
+          style={{
+            backgroundColor: PRIMARY_COLOR,
+            height: `${value}px`,
+          }}
+        ></div>
+      ))}
+      {/* Notice how we only pass the name of the function to the event handler and
+      not the name followed by parentheses. Parentheses after a function name
+      executes the function. In other words, if we passed in the function with
+      parentheses, the function will execute every time the component renders.
+      We do not want to execute a function inside of the event handler. */}
+      <button onClick={resetArray}>Generate New Array</button>
+      <button onClick={mergeSort}>Merge Sort</button>
+      <button onClick={() => this.quickSort()}>Quick Sort</button>
+      <button onClick={() => this.heapSort()}>Heap Sort</button>
+      <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+      <button onClick={testSortingAlgorithms}>
+        Test Sorting Algorithms (BROKEN)
+      </button>
+    </div>
+  );
 }
 
 // From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
