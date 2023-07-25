f59c7c9779d67bf13227d60b61a3f5d4c101b68d
Shivraj <shivrajofficial102@gmail.com>
2023-07-25 16:37:02 +0530

    Seprating constants

---
+++ src/lable.js
@@ -0,0 +1,12 @@
+// Description: This file contains the description of each algorithm. 
+export const constant = {
+    fixedSpeed : 140,
+    mergeSort:
+        "Merge sort is a sorting algorithm that works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.",
+    HeapSort:
+        "Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.",
+    QuickSort:
+        "QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around the picked pivot. There are many different versions of quickSort that pick pivot in different ways.",
+    BubbleSort:
+        "Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.",
+};
